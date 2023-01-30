6e10bb74ce555e3f65a65fa3a44a040e7454dc63
Charley-YH-Wu <105037202+Charley-YH-Wu@users.noreply.github.com>
2023-01-30 12:35:17 -0800

    four

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
-const TRACKING_ID = "G-NG1EHQV4BY"; // OUR_TRACKING_ID
+const TRACKING_ID = "UA-254143119-2"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
