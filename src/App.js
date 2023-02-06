de26a6bf31f96290702fce5ccce1c5c78c6e171e
Charley-YH-Wu <105037202+Charley-YH-Wu@users.noreply.github.com>
2023-02-06 14:27:42 -0800

    c

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga4';
-const TRACKING_ID = "UG-NG1EHQV4BY"; // OUR_TRACKING_ID
+const TRACKING_ID = "G-NG1EHQV4BY"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 const useAnalyticsEventTracker = (category="Blog category") => {
