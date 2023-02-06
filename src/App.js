3288d61c40eb8125c42f43241e09a9665ca8dcdd
Charley-YH-Wu <105037202+Charley-YH-Wu@users.noreply.github.com>
2023-02-06 00:05:41 -0800

    h

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import ReactGA from 'react-ga';
 const TRACKING_ID = "UA-254143119-2"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
+const useAnalyticsEventTracker = (category="Blog category") => {
+  const eventTracker = (action = "test action", label = "test label") => {
+    ReactGA.event({category, action, label});
+  }
+  return eventTracker;
+}
+
 function App() {
   ReactGA.pageview(window.location.pathname+window.location.search);
+  const gaEventTracker = useAnalyticsEventTracker('Learn more');
   return (
     <div className="App">
       <header className="App-header">
@@ -15,15 +23,17 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://charley-yh-wu.github.io/react-ga/"
+          onClick={()=>gaEventTracker('call')}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
